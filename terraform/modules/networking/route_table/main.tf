95d2f1b624a0c1da97c889e8cfd5db1aa0e85de3
DinoJelincic <djelincic00@gmail.com>
2025-07-02 16:35:39 +0200

    fix error

--- terraform/modules/networking/route_table/main.tf
+++ terraform/modules/networking/route_table/main.tf
@@ -2,7 +2,7 @@ resource "aws_route_table" "route_table" {
   vpc_id = var.vpc_id
 
   route {
-    cidr_block     = var.cidr_block
+    cidr_block     = "0.0.0.0/0"
     gateway_id     = var.gateway_id
     nat_gateway_id = var.nat_gateway_id
   }
