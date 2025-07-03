eae3d6462203ec6674b1dfd3b9463bd2d9331b0b
DinoJelincic <djelincic00@gmail.com>
2025-07-03 20:18:29 +0200

    add ec2 module

--- terraform/modules/compute/ec2/outputs.tf
+++ terraform/modules/compute/ec2/outputs.tf
@@ -1,4 +1,8 @@
-output "private_key_pem" {
-  value       = tls_private_key.ec2_key.private_key_pem
-  sensitive   = true
+output "id" {
+  value = aws_instance.app_instance.id
+  
+}
+output "arn" {
+  value = aws_instance.app_instance.arn
+  
 }
