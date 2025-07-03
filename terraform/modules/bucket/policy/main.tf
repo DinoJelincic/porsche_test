b75537d6416c96c98220338a59032c8605daf5ea
DinoJelincic <djelincic00@gmail.com>
2025-07-03 11:54:19 +0200

    remove s3 policy

--- terraform/modules/bucket/policy/main.tf
+++ terraform/modules/bucket/policy/main.tf
@@ -1,28 +1,28 @@
-resource "aws_s3_bucket_policy" "restricted_policy" {
-  bucket = var.bucket_id
+# resource "aws_s3_bucket_policy" "restricted_policy" {
+#   bucket = var.bucket_id
 
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid = "AllowTerraformRoleFullAccess",
-        Effect = "Allow",
-        Principal = {
-          AWS = var.terraform_role_arn
-        },
-        Action = [
-          "s3:GetBucketPolicy",
-          "s3:PutBucketPolicy",
-          "s3:GetObject",
-          "s3:ListBucket",
-          "s3:DeleteObject",
-          "s3:PutObject"
-        ],
-        Resource = [
-          var.bucket_arn,
-          "${var.bucket_arn}/*"
-        ]
-      }
-    ]
-  })
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Sid = "AllowTerraformRoleFullAccess",
+#         Effect = "Allow",
+#         Principal = {
+#           AWS = var.terraform_role_arn
+#         },
+#         Action = [
+#           "s3:GetBucketPolicy",
+#           "s3:PutBucketPolicy",
+#           "s3:GetObject",
+#           "s3:ListBucket",
+#           "s3:DeleteObject",
+#           "s3:PutObject"
+#         ],
+#         Resource = [
+#           var.bucket_arn,
+#           "${var.bucket_arn}/*"
+#         ]
+#       }
+#     ]
+#   })
+# }
