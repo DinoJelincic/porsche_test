5360b298a87eddbbca4367cf0219bbcdc73f38ce
DinoJelincic <djelincic00@gmail.com>
2025-07-03 10:03:25 +0200

    fix sw3 policy

--- terraform/modules/bucket/policy/main.tf
+++ terraform/modules/bucket/policy/main.tf
@@ -5,6 +5,25 @@ resource "aws_s3_bucket_policy" "restricted_policy" {
     Version = "2012-10-17",
     Statement = [
       {
+        Sid = "AllowTerraformRoleFullAccess",
+        Effect = "Allow",
+        Principal = {
+          AWS = var.terraform_role_arn
+        },
+        Action = [
+          "s3:GetBucketPolicy",
+          "s3:PutBucketPolicy",
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:DeleteObject",
+          "s3:PutObject"
+        ],
+        Resource = [
+          var.bucket_arn,
+          "${var.bucket_arn}/*"
+        ]
+      },
+      {
         Sid = "AccessFromVpcOnly",
         Effect = "Deny",
         Principal = "*",
