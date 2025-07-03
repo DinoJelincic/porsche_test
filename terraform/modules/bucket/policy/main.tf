a2a69059b341783cc27171ebcbfd76fbf3ade7fa
DinoJelincic <djelincic00@gmail.com>
2025-07-03 10:01:28 +0200

    reset sw3 policy

--- terraform/modules/bucket/policy/main.tf
+++ terraform/modules/bucket/policy/main.tf
@@ -5,7 +5,7 @@ resource "aws_s3_bucket_policy" "restricted_policy" {
     Version = "2012-10-17",
     Statement = [
       {
-        Sid = "DenyNonVpcEndpointAccess",
+        Sid = "AccessFromVpcOnly",
         Effect = "Deny",
         Principal = "*",
         Action = "s3:*",
@@ -18,30 +18,6 @@ resource "aws_s3_bucket_policy" "restricted_policy" {
             "aws:SourceVpce" = var.vpc_endpoint_id
           }
         }
-      },
-      {
-        Sid = "AllowTerraformRoleFullAccess",
-        Effect = "Allow",
-        Principal = {
-          AWS = var.terraform_role_arn
-        },
-        Action = "s3:*",
-        Resource = [
-          var.bucket_arn,
-          "${var.bucket_arn}/*"
-        ]
-      },
-      {
-        Sid = "AllowTerraformRolePolicyManagement",
-        Effect = "Allow",
-        Principal = {
-          AWS = var.terraform_role_arn
-        },
-        Action = [
-          "s3:GetBucketPolicy",
-          "s3:PutBucketPolicy"
-        ],
-        Resource = var.bucket_arn
       }
     ]
   })
