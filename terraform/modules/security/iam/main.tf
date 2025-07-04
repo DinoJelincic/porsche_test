d13e79c3ee5689a55cf65e4a5895374021e72c62
DinoJelincic <djelincic00@gmail.com>
2025-07-04 12:58:44 +0200

    modify iam policy

--- terraform/modules/security/iam/main.tf
+++ terraform/modules/security/iam/main.tf
@@ -14,27 +14,40 @@ resource "aws_iam_role" "ec2_role" {
 }
 
 resource "aws_iam_role_policy" "ec2_policy" {
-  name = "ec2_s3_access_policy"
+  name = "ec2_s3_ecr_access_policy"
   role = aws_iam_role.ec2_role.id
 
   policy = jsonencode({
     Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Action = [
-        "s3:GetObject",
-        "s3:PutObject",
-        "s3:DeleteObject",
-        "s3:ListBucket",
-        "s3:GetBucketPolicy"
-      ],
-      Resource = [
-        var.bucket_arn,
-        "${var.bucket_arn}/*"
-      ]
-    }]
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:ListBucket",
+          "s3:GetBucketPolicy"
+        ],
+        Resource = [
+          var.bucket_arn,
+          "${var.bucket_arn}/*"
+        ]
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage"
+        ],
+        Resource = "*"
+      }
+    ]
   })
 }
+
 
 resource "aws_iam_instance_profile" "instance_profile" {
   name = "ec2_instance_profile"
